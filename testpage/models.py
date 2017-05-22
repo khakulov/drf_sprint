c30c48696abf4c4ea22245d0347a09be5443e217
Matt Davis <matteius@gmail.com>
2017-05-22 17:27:06 -0400

    Add new model for a related field

--- testpage/models.py
+++ testpage/models.py
@@ -4,9 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-# Create your models here.
+class House(models.Model):
+    location = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+
+
 class Animal(models.Model):
     family = models.CharField(max_length=30)
     email_address = models.EmailField(unique=True)
     name = models.CharField(max_length=32, unique=True)
     secret_key = models.CharField(max_length=32, unique=True)
+    house = models.ForeignKey(House, null=True)
