8c5583d50bd944f26babd0d84c650670fdc62db0
Brian Adams <brian.jacob.adams@gmail.com>
2017-05-22 11:47:52 -0700

    adding base model view

--- sprint/views.py
+++ sprint/views.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from models import Programmer
+from serializers import ProgrammerSerializer
+
 
 # Create your views here.
+class ProgrammerViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
+    permission_classes = []
