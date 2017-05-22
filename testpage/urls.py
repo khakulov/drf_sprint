eb75a2f916d0e099c8f3971d5b1bd8ea8897d105
Azim Khakulov <azim@nwebs.de>
2017-05-23 00:01:39 +0200

    added coreapi support

--- testpage/urls.py
+++ testpage/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from django.views.decorators.cache import cache_control
+from rest_framework.schemas import get_schema_view
 
 from .views import ListAnimals
+
+schema_view = get_schema_view(title='Animals API')
 
 cached = cache_control(private=True, maxage=5 * 60 * 1)
 
 urlpatterns = [
+    url('^$', schema_view),
     url(r'^animals/$',
         ListAnimals.as_view(), name="list_animals"),
 ]
